d497281f6654eefa77691366c7126e8c05c511c6
Drex <120832806+drexiing@users.noreply.github.com>
2026-01-06 15:26:37 -0300

    Update init imports

--- bcscraper/__init__.py
+++ bcscraper/__init__.py
@@ -1,3 +1,5 @@
 from .classes import Curso, Modulo
-from .main import (obtener_cursos, buscar_sigla, buscar_profesor,
-                   buscar_curso, obtener_curso, obtener_periodos)
+from .main import (
+    obtener_cursos, buscar_por_sigla, buscar_por_profesor,
+    buscar_por_nrc, buscar_por_nombre, buscar_curso, obtener_periodos
+)
